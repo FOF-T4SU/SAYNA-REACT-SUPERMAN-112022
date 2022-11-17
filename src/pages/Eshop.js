f30790a6658fca466b75f3015bd63073b762560b
FOF-T4SU <fof.t4su.sayna@gmail.com>
2022-11-17 04:09:15 +0000

    mise en place des assets, components et pages

---
+++ src/pages/Eshop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Eshop() {
+  return <div>Eshop</div>;
+}
+
+export default Eshop;
---
+++ src/pages/MonCompte.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MonCompte() {
+  return <div>MonCompte</div>;
+}
+
+export default MonCompte;
